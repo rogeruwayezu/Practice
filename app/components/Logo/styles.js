9471774f6f3ae1327b1204db904b8e4f751d6bc8
rogeruwayezu <rogeruwayezu@gmail.com>
2018-02-27 10:48:45 +0200

    finished UI

---
+++ app/components/Logo/styles.js
@@ -0,0 +1,32 @@
+import { Dimensions } from 'react-native'
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+const imageWidth = Dimensions.get('window').width / 2;
+
+export default EStyleSheet.create({
+   $largeContainerSize: imageWidth,
+   $largeImageSize: imageWidth / 2,
+   $smallContainerSize: imageWidth / 2,
+   $smallImageSize: imageWidth / 4,
+   container:{
+       alignItems: 'center',
+   },
+   containerImage:{
+       alignItems: 'center',
+       justifyContent: 'center',
+       width: '$largeContainerSize',
+       height: '$largeContainerSize',           
+   },
+   
+   logo: {
+       width: '$largeImageSize',
+       tintColor: '$primaryBlue',
+   },
+   text: {
+       fontWeight: '600',
+       fontSize: 28,
+       letterSpacing: 2,
+       color: '$white',
+       paddingTop: 15,
+   },
+});
---
+++ app/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+import Themes from './screens/Themes';
+
+EStyleSheet.build({
+    $primaryOrange: '#D57A66',
+    $primaryGreen: '#00BD9D',
+    $primaryPurple: '#9E768F',
+    $primaryBlue: '#4F6D7A',
+    
+    $white: '#ffff',
+    $border: '#E2E2E2',
+    $inputText: '#797979',
+    $lightGray: '#F0F0F0',
+    $darkText: '#343434',
+});
+
+export default () => <Themes />;
