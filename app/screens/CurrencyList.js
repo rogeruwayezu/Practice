9426ebb1638178d4d9e60f347333d67d67f57bad
rogeruwayezu <rogeruwayezu@gmail.com>
2018-02-28 18:53:42 +0200

    currencyList actions

--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import { Text, FlatList, View, StatusBar } from 'react-native';
+import { connect } from 'react-redux';
+
 import PropTypes from 'prop-types';
 import { ListItem, Separator } from '../components/List';
 import currencies from '../data/currencies';
+import { changeBaseCurrency, changeQuoteCurrency } from '../actions/currencies';
+
+
 
 const TEMP_CURRENT_CURRENCY = 'CAD';
 
 class CurrencyList extends Component {
     static propTypes = {
         navigation: PropTypes.object,
-    }
-    handlePress = () => {
-       this.props.navigation.goBack(null);
+        dispatch: PropTypes.func,
     };
+
+    handlePress = (currency) => {
+        const { type } = this.props.navigation.state.params;
+        if (type === 'base') {
+          this.props.dispatch(changeBaseCurrency(currency));
+        } else if (type === 'quote') {
+          this.props.dispatch(changeQuoteCurrency(currency));
+        }
+    
+        this.props.navigation.goBack(null);
+      };
 
     render() {
         return (
@@ -24,7 +38,7 @@ class CurrencyList extends Component {
                     renderItem={({ item }) => ( 
                     <ListItem text={item} 
                        selected={item === TEMP_CURRENT_CURRENCY}
-                       onPress={this.handlePress}
+                       onPress={() => this.handlePress(item)}
                       />
                     )}
                       keyExtractor={item => item}
@@ -35,4 +49,4 @@ class CurrencyList extends Component {
     }
 }
 
-export default CurrencyList;
+export default connect()(CurrencyList);
